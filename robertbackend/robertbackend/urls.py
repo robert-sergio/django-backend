9375e7c5795653a846afc464e892015511f6fb46
Robert <robert.sergio.eng@gmail.com>
2023-04-06 08:10:05 -0300

    add first app

--- robertbackend/robertbackend/urls.py
+++ robertbackend/robertbackend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('core/', include('core.urls')),
     path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
 ]
